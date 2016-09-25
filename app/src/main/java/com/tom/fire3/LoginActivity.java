12cd4744235053c9e900ff7f507d35d901fbab11
Hank Tom <hanktom@gmail.com>
2016-09-25 14:03:21 +0800

    auth與傾聽器屬性

--- app/src/main/java/com/tom/fire3/LoginActivity.java
+++ app/src/main/java/com/tom/fire3/LoginActivity.java
@@ -1,24 +1,47 @@
 package com.tom.fire3;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText edUserid;
     private EditText edPasswd;
+    FirebaseAuth auth;
+    FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         findViews();
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
     }
 
     public void login(View v){
-        
+//        auth.
     }
 
     private void findViews() {
@@ -26,3 +49,13 @@ public class LoginActivity extends AppCompatActivity {
         edPasswd = (EditText) findViewById(R.id.passwd);
     }
 }
+
+
+
+
+
+
+
+
+
+
