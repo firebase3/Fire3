ac0d7505aa13ee1099f579ca904cdd4c90e827ca
Hank Tom <hanktom@gmail.com>
2016-10-02 11:00:26 +0800

    Facebook Login

--- app/src/main/java/com/tom/fire3/LoginActivity.java
+++ app/src/main/java/com/tom/fire3/LoginActivity.java
@@ -1,24 +1,37 @@
 package com.tom.fire3;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
     private EditText edUserid;
     private EditText edPasswd;
     FirebaseAuth auth;
@@ -29,6 +42,11 @@ public class LoginActivity extends AppCompatActivity {
             if ( user != null ){
                 Toast.makeText(LoginActivity.this,
                         "Login Successful", Toast.LENGTH_LONG).show();
+                getSharedPreferences(getString(R.string.pref_name), MODE_PRIVATE)
+                        .edit()
+                        .putString(getString(R.string.pref_uid), user.getUid())
+                        .putString(getString(R.string.pref_email), user.getEmail())
+                        .apply();
                 finish();
             }
         }
@@ -36,14 +54,39 @@ public class LoginActivity extends AppCompatActivity {
     //google
     GoogleSignInOptions gso;
     GoogleApiClient googleApiClient;
-
-
+    private CallbackManager callbackManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
         setContentView(R.layout.activity_login);
+        //facebook
+        LoginButton fbLogin = (LoginButton) findViewById(R.id.button_facebook_login);
+        fbLogin.setReadPermissions("email", "public_profile");
+        callbackManager = CallbackManager.Factory.create();
+        fbLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess");
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError");
+            }
+        });
+
+        //google
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .requestIdToken(getString(R.string.web_application_id))
@@ -53,6 +96,29 @@ public class LoginActivity extends AppCompatActivity {
 
         findViews();
         auth = FirebaseAuth.getInstance();
+    }
+
+    private void handleFacebookToken(AccessToken accessToken) {
+        AuthCredential credential =
+            FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String message = "Facebook logon";
+                        if (!task.isSuccessful()){
+                            message = "Facebook login failed";
+                        }
+                        Toast.makeText(LoginActivity.this,
+                                message, Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
